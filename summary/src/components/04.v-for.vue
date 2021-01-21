710cef151034a79a936dc54c9158042764865302
18362892942@163.com <18362892942@163.com>
2021-01-21 21:55:18 +0800

    v-for

---
+++ summary/src/components/04.v-for.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div v-for="(name,index) in names" :key="name">
+            {{index}} --- {{ name }} 
+        </div>
+        <div v-for="(value,key,index) in objs" :key="key">
+            {{ value }} --- {{key}} --- {{index}}
+        </div>
+        <div v-for="num in nums" :key="num">
+            {{ num }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                names: ["jack",'box'],
+                objs: {
+                    name:"jack",
+                    age: 12
+                },
+                nums: 10
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
