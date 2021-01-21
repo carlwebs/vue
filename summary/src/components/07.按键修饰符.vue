b7ad24352d7ce6dcf74fc3699e191fe34c299174
18362892942@163.com <18362892942@163.com>
2021-01-21 22:20:02 +0800

    按键修饰符

---
+++ summary/src/components/07.按键修饰符.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <input v-model="msg" type="text" @keyup.enter="keyCodeDemo" />
+        <input v-model="msg" type="text" @keyup.13="keyCodeDemo" />
+        <input v-model="msg" type="text" @keyup.f2="keyCodeDemo" />
+    </div>
+</template>
+
+<script>
+    import Vue from 'vue';
+    // 定义keycode的别名
+    Vue.config.keyCodes.f2 = 113;
+    export default {
+        data() {
+            return {
+                msg: ""
+            }
+        },
+        methods: {
+            keyCodeDemo() {
+                console.log("keyCode");
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
