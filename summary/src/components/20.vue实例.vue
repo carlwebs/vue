24051567d39504f0a4db1cce66f5066fa2a33e19
18362892942@163.com <18362892942@163.com>
2021-03-08 22:51:16 +0800

    vue实例简单介绍

---
+++ summary/src/components/20.vue实例.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    1.vue框架无非就是Vue构造函数创建的一个实例。
+    2.vue中的数据只有在创建的时候被添加到data中的才是响应式的。
+    3.在生命周期中不用使用箭头函数，箭头函数中的this指向定义的地方，这里不是指向vue实例对象。
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
